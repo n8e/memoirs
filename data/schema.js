8036fc9c8cb0a7da1609fb6180ff2ab61a594f00
n8e <godmetweenciati@gmail.com>
2017-01-05 00:24:13 +0300

    Root schema definition

---
+++ data/schema.js
@@ -0,0 +1,9 @@
+import { GraphQLSchema } from 'graphql';
+
+import RootType from './types/RootType';
+import RootMutation from './mutations/RootMutation';
+
+export var Schema = new GraphQLSchema({
+  query: RootType,
+  mutation: RootMutation
+});
