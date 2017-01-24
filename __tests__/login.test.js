d14dc8d39f88e01459ec9214a5ccfc32db9baec1
n8e <godmetweenciati@gmail.com>
2017-01-24 20:50:54 +0300

    Updated login view tests with initial component state tests

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -4,18 +4,29 @@ import TestUtils from 'react-addons-test-utils'
 import Relay from '../__mocks__/react-relay.js';
 import LoginView from '../js/views/login/LoginView';
 
-test('LoginView', () => {
-  const fixtures = {
-    viewer: {
-      userInfo: {
-        userName: 'Nate'
-      }
+const fixtures = {
+  viewer: {
+    userInfo: {
+      userName: 'Nate'
     }
   }
+}
 
-  const result = TestUtils.renderIntoDocument(
-    <LoginView {...fixtures} />
-  );
+const result = TestUtils.renderIntoDocument(
+  <LoginView {...fixtures} />
+);
 
+test('LoginView initial state', () => {
   expect(result.state.fail).toBe('none');
+  expect(result.state.success).toBe('none');
+  expect(result.state.user.username).toBe(null);
+  expect(result.state.user.password).toBe(null);
+});
+
+test('LoginView functions', () => {
+  expect(result.onSuccess).toBeDefined();
+  expect(typeof result.onSuccess).toBe('function');
+  expect(result.onFailure).toBeDefined();
+  expect(result.handleChange).toBeDefined();
+  expect(result.handleLogin).toBeDefined();
 });
