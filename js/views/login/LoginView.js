f425b94eadc5f5860be5f58790cc30832189915f
n8e <godmetweenciati@gmail.com>
2017-02-10 21:41:25 +0300

    Refactors loginview

--- js/views/login/LoginView.js
+++ js/views/login/LoginView.js
@@ -1,57 +1,82 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
 import Relay from 'react-relay';
 import { Grid, Row, Col, Form, FormGroup, FormControl, Checkbox, Button, Panel, ControlLabel, Alert } from 'react-bootstrap';
 import { Link, hashHistory } from 'react-router';
 
 import LoginMutation from '../../mutations/LoginMutation';
 
-class LoginView extends Component {
-  constructor(props) {
-    super(props);
+const titleCase = (text) => text.replace(/(^\w|\b\w)/g, (m) => m.toUpperCase());
 
-    this.state = {
-      fail: 'none',
-      success: 'none',
-      user: {
-        username: null,
-        password: null
-      }
-    };
+class LoginView extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			fail: 'none',
+			success: 'none',
+			user: {
+				username: null,
+				password: null
+			}
+		};
 
     // bind methods
-    this.onSuccess = this.onSuccess.bind(this);
-    this.onFailure = this.onFailure.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-  }
+		this.onSuccess = this.onSuccess.bind(this);
+		this.onFailure = this.onFailure.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleLogin = this.handleLogin.bind(this);
+		this.renderFormFields = this.renderFormFields.bind(this);
+	}
 
-  onSuccess(res) {
-    console.log('Response on view', res.login.userInfo);
-    this.setState({success: 'block'});
-    // implement transition
-    hashHistory.push('/welcome');
-  }
+	onSuccess(res) {
+		console.log('Response on view', res.login.userInfo);
+		this.setState({success: 'block'});
+		hashHistory.push('/welcome');
+	}
 
-  onFailure() {
-    this.setState({fail: 'block'});
-  }
+	onFailure() {
+		this.setState({fail: 'block'});
+	}
 
-  handleChange(e) {
-    this.setState({user: Object.assign({}, this.state.user, {[e.target.name]: e.target.value})});
-  }
+	handleChange(e) {
+		const { name, value } = e.target;
+		this.setState({user: Object.assign({}, this.state.user, {[name]: value})});
+	}
 
-  handleLogin() {
-    const onSuccess = this.onSuccess;
-    const onFailure = this.onFailure;
-    Relay.Store.commitUpdate(new LoginMutation({
-      username: this.state.user.username,
-      password: this.state.user.password,
-      viewer: this.props.viewer
-    }), {onFailure, onSuccess});
-  }
+	handleLogin() {
+		const onSuccess = this.onSuccess;
+		const onFailure = this.onFailure;
+		Relay.Store.commitUpdate(new LoginMutation({
+			username: this.state.user.username,
+			password: this.state.user.password,
+			viewer: this.props.viewer
+		}), {onFailure, onSuccess});
+	}
 
-  render() {
-    return (
+	renderFormFields() {
+		const fields = ['username', 'password'];
+		return fields.map((field, i) => {
+			let fieldTitleCase = titleCase(field);
+			return (
+				<FormGroup key={i} controlId={`formHorizontal${fieldTitleCase}`}>
+					<Col componentClass={ControlLabel} sm={2}>
+						{fieldTitleCase}
+					</Col>
+					<Col sm={10}>
+						<FormControl
+							type={(field === 'password') ? 'password' : 'text'}
+							name={field}
+							placeholder={fieldTitleCase}
+							onChange={this.handleChange}
+						/>
+					</Col>
+				</FormGroup>
+			);
+		});
+	}
+
+	render() {
+		return (
       <Grid>
         <Row>
           <Panel>
@@ -59,31 +84,14 @@ class LoginView extends Component {
           </Panel>
         </Row>
         <Row>
-          <Alert bsStyle="danger" ref="failAlert" style={{display: this.state.fail}}>
+          <Alert bsStyle='danger' ref='failAlert' style={{display: this.state.fail}}>
             <strong>Login failure!</strong> Please try again.
           </Alert>
-          <Alert bsStyle="success" ref="successAlert" style={{display: this.state.success}}>
-            <strong>Login successful</strong>
+          <Alert bsStyle='success' ref='successAlert' style={{display: this.state.success}}>
+            <strong>Login successful!</strong>
           </Alert>
           <Form horizontal>
-            <FormGroup controlId="formHorizontalUsername">
-              <Col componentClass={ControlLabel} sm={2}>
-                Username
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" name="username" placeholder="Username" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
-            <FormGroup controlId="formHorizontalPassword">
-              <Col componentClass={ControlLabel} sm={2}>
-                Password
-              </Col>
-              <Col sm={10}>
-                <FormControl type="password" name="password" placeholder="Password" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
+						{this.renderFormFields()}
             <FormGroup>
               <Col smOffset={2} sm={10}>
                 <Checkbox>Remember me</Checkbox>
@@ -99,18 +107,18 @@ class LoginView extends Component {
             </FormGroup>
 
             <Col smOffset={2} sm={10}>
-              <Link to={`/create`}>Sign up link</Link>
+              <Link to={'/create'}>Sign up link</Link>
             </Col>
           </Form>
         </Row>
       </Grid>
-    )
-  }
+		);
+	}
 }
 
 export default Relay.createContainer(LoginView, {
-  fragments: {
-    viewer: () => Relay.QL`
+	fragments: {
+		viewer: () => Relay.QL`
       fragment on User {
         ${LoginMutation.getFragment('viewer')},
         userInfo {
@@ -118,5 +126,5 @@ export default Relay.createContainer(LoginView, {
         }
       }
     `,
-  }
+	}
 });
