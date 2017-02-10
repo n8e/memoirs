6e2d22a60c0248e1e43487a703f842492a38b053
n8e <godmetweenciati@gmail.com>
2017-02-10 21:58:11 +0300

    Sign up form refactors

--- js/views/user/CreateUserView.js
+++ js/views/user/CreateUserView.js
@@ -4,57 +4,78 @@ import { Grid, Row, Col, Form, FormGroup, FormControl, Checkbox, Button, Panel, 
 
 import SignUpMutation from '../../mutations/SignUpMutation';
 
+const camelToTitleCase = (text) => text
+  .replace(/([A-Z])/g, ' $1')
+  .replace(/^./, str => str.toUpperCase());
+
 class CreateUserView extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+		this.state = {
+			fail: 'none',
+			success: 'none',
+			user: {
+				firstName: null,
+				lastName: null,
+				userName: null,
+				email: null,
+				password: null
+			}
+		};
 
-    this.state = {
-      fail: 'none',
-      success: 'none',
-      user: {
-        firstName: null,
-        lastName: null,
-        userName: null,
-        email: null,
-        password: null
-      }
-    };
+		this.onSuccess = this.onSuccess.bind(this);
+		this.onFailure = this.onFailure.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSignUp = this.handleSignUp.bind(this);
+		this.renderFormFields = this.renderFormFields.bind(this);
+	}
 
-    // bind methods
-    this.onSuccess = this.onSuccess.bind(this);
-    this.onFailure = this.onFailure.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-  }
+	renderFormFields() {
+		const fields = ['firstName', 'lastName', 'userName', 'email', 'password'];
+		return fields.map((field, i) => {
+			let fieldTitleCase = camelToTitleCase(field);
+			return (
+				<FormGroup key={i} controlId={`formHorizontal${fieldTitleCase}`}>
+					<Col componentClass={ControlLabel} sm={2}>
+						{fieldTitleCase}
+					</Col>
+					<Col sm={10}>
+						<FormControl
+							type={(field === 'password') ? 'password' : 'text'}
+							name={field}
+							placeholder={fieldTitleCase}
+							onChange={this.handleChange}
+						/>
+					</Col>
+				</FormGroup>
+			);
+		});
+	}
 
-  onSuccess(res) {
-    console.log('Response at View', res.signUp.signUpInfo);
-    this.setState({success: 'block'});
-  }
+	onSuccess() { this.setState({success: 'block'}); }
 
-  onFailure() {
-    this.setState({fail: 'block'});
-  }
+	onFailure() { this.setState({fail: 'block'}); }
 
-  handleChange(e) {
-    this.setState({user: Object.assign({}, this.state.user, {[e.target.name]: e.target.value})});
-  }
+	handleChange(e) {
+		const { name, value } = e.target;
+		this.setState({user: Object.assign({}, this.state.user, {[name]: value})});
+	}
 
-  handleSignUp() {
-    const onSuccess = this.onSuccess;
-    const onFailure = this.onFailure;
-    Relay.Store.commitUpdate(new SignUpMutation({
-      firstName: this.state.user.firstName,
-      lastName: this.state.user.lastName,
-      userName: this.state.user.userName,
-      email: this.state.user.email,
-      password: this.state.user.password,
-      viewer: this.props.viewer
-    }), {onFailure, onSuccess});
-  }
+	handleSignUp() {
+		const onSuccess = this.onSuccess;
+		const onFailure = this.onFailure;
+		Relay.Store.commitUpdate(new SignUpMutation({
+			firstName: this.state.user.firstName,
+			lastName: this.state.user.lastName,
+			userName: this.state.user.userName,
+			email: this.state.user.email,
+			password: this.state.user.password,
+			viewer: this.props.viewer
+		}), {onFailure, onSuccess});
+	}
 
-  render() {
-    return (
+	render() {
+		return (
       <Grid>
         <Row>
           <Panel>
@@ -66,57 +87,13 @@ class CreateUserView extends Component {
             <strong>Sign up failure!</strong> Please try again.
           </Alert>
           <Alert bsStyle="success" ref="successAlert" style={{display: this.state.success}}>
-            <strong>Sign up successful</strong>
+            <strong>Sign up successful!</strong>
           </Alert>
           <div>
-            <FormGroup controlId="formHorizontalFirstName">
-              <Col componentClass={ControlLabel} sm={2}>
-                First Name
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
-            <FormGroup controlId="formHorizontalLastName">
-              <Col componentClass={ControlLabel} sm={2}>
-                Last Name
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
-            <FormGroup controlId="formHorizontalUserName">
-              <Col componentClass={ControlLabel} sm={2}>
-                User Name
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" name="userName" placeholder="User Name" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
-            <FormGroup controlId="formHorizontalEmail">
-              <Col componentClass={ControlLabel} sm={2}>
-                Email
-              </Col>
-              <Col sm={10}>
-                <FormControl type="text" name="email" placeholder="Email" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
-            <FormGroup controlId="formHorizontalPassword">
-              <Col componentClass={ControlLabel} sm={2}>
-                Password
-              </Col>
-              <Col sm={10}>
-                <FormControl type="password" name="password" placeholder="Password" onChange={this.handleChange} />
-              </Col>
-            </FormGroup>
-
+            { this.renderFormFields() }
             <FormGroup>
               <Col smOffset={2} sm={10}>
-                <Button onClick={this.handleSignUp}>
+                <Button onClick={ this.handleSignUp }>
                   Sign up
                 </Button>
               </Col>
@@ -124,13 +101,13 @@ class CreateUserView extends Component {
           </div>
         </Row>
       </Grid>
-    )
-  }
+		);
+	}
 }
 
 export default Relay.createContainer(CreateUserView, {
-  fragments: {
-    viewer: () => Relay.QL`
+	fragments: {
+		viewer: () => Relay.QL`
       fragment on User {
         ${SignUpMutation.getFragment('viewer')},
         signUpInfo{
@@ -138,5 +115,5 @@ export default Relay.createContainer(CreateUserView, {
         },
       }
     `,
-  }
+	}
 });
