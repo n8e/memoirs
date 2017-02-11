9389aca63e405ae8f15a5ead77d4990cae499e11
n8e <godmetweenciati@gmail.com>
2017-02-11 22:09:07 +0300

    Refactors to include auth module for authentication

--- js/views/common/Navigator.js
+++ js/views/common/Navigator.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import Relay from 'react-relay';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
+import { clearLoginObj } from '../../reusable/auth';
+
 class Navigator extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      name: this.props.userInfo.userName,
-    }
-  }
+		this.doLogout = this.doLogout.bind(this);
+	}
 
-  render() {
-    console.log('Navigator props', this.props);
-    return (
+	doLogout() {
+		clearLoginObj();
+	}
+
+	render() {
+		return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
@@ -22,23 +25,15 @@ class Navigator extends Component {
         </Navbar.Header>
         <Nav>
           <NavItem eventKey={1} href="#">Memoirs</NavItem>
-          <NavDropdown eventKey={3} title={this.state.name} id="basic-nav-dropdown">
+          <NavDropdown eventKey={3} title={this.props.userInfo.name} id="basic-nav-dropdown">
             <MenuItem eventKey={3.3}>Create Memoir</MenuItem>
             <MenuItem divider />
-            <MenuItem eventKey={3.3}>Logout</MenuItem>
+            <MenuItem href="#/login" onClick={this.doLogout}>Logout</MenuItem>
           </NavDropdown>
         </Nav>
       </Navbar>
-    )
-  }
+		);
+	}
 }
 
-export default Relay.createContainer(Navigator, {
-  fragments: {
-    userInfo: () => Relay.QL`
-      fragment on UserInfo {
-        userName
-      }
-    `,
-  }
-})
+export default Navigator;
--- js/views/container/App.js
+++ js/views/container/App.js
@@ -2,32 +2,28 @@ import React, { Component } from 'react';
 import Relay from 'react-relay';
 
 import Navigator from '../common/Navigator';
+import { getUserInfo } from '../../reusable/auth';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-  }
+	constructor(props){
+		super(props);
+		this.state = {
+			userInfo: {
+				name: getUserInfo().userName,
+				loggedIn: getUserInfo().loggedIn,
+				token: getUserInfo().token,
+			}
+		};
+	}
 
-  render() {
-    console.log('viewer in App', this.props.viewer);
-    return (
+	render() {
+		return (
       <div className="App" id="root">
-        <Navigator userInfo={this.props.viewer.userInfo} />
+        <Navigator userInfo={this.state.userInfo} />
         { React.cloneElement(this.props.children, {/*props*/}) }
       </div>
-    );
-  }
+		);
+	}
 }
 
-// export default App;
-export default Relay.createContainer(App, {
-  fragments: {
-    viewer: () => Relay.QL`
-      fragment on User {
-        userInfo {
-          ${Navigator.getFragment('userInfo')}
-        }
-      }
-    `,
-  }
-})
+export default App;
