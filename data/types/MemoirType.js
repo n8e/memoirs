4b59a44870eb014ebc4f23b71671928006c8f4b1
n8e <godmetweenciati@gmail.com>
2017-02-14 10:55:53 +0300

    Added memoirs connection and memoir type and model

---
+++ data/types/MemoirType.js
@@ -0,0 +1,29 @@
+import { GraphQLString, GraphQLInt, GraphQLObjectType } from 'graphql';
+
+import { globalIdField, connectionDefinitions } from 'graphql-relay';
+
+const MemoirType = new GraphQLObjectType({
+	name: 'Memoir',
+	fields: () => ({
+		id: globalIdField('Memoir'),
+		memoirId: { type: GraphQLString },
+		title: { type: GraphQLString },
+		content: { type: GraphQLString },
+	})
+});
+
+const connection = connectionDefinitions({
+	name: 'Memoir',
+	nodeType: MemoirType,
+	connectionFields: () => ({
+		totalCount: {
+			type: GraphQLInt,
+			resolve: conn => conn.edges.length,
+		}
+	}),
+});
+
+export const MemoirConnection = connection.connectionType;
+export const MemoirEdge = connection.edgeType;
+
+export default MemoirType;
---
+++ data/types/MemoirsType.js
@@ -0,0 +1,18 @@
+import { GraphQLObjectType } from 'graphql';
+import { globalIdField, connectionArgs, connectionFromArray } from 'graphql-relay';
+
+import { MemoirConnection } from './MemoirType';
+
+const MemoirsType = new GraphQLObjectType({
+	name:'Memoirs',
+	fields: () => ({
+		id: globalIdField('Memoirs'),
+		items: {
+			type: MemoirConnection,
+			args: connectionArgs,
+			resolve: (obj, args) => connectionFromArray(obj.memoirs, args),
+		}
+	}),
+});
+
+export default MemoirsType;
