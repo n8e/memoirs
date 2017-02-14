0a51d44f47ed4c46ace34749165bbbb548ce9b82
n8e <godmetweenciati@gmail.com>
2017-02-14 18:56:25 +0300

    Eslint formatting

--- data/types/RootType.js
+++ data/types/RootType.js
@@ -4,13 +4,13 @@ import { EmptyUser } from '../remote/models/User';
 import UserType from './UserType';
 
 const RootType = new GraphQLObjectType({
-  name: 'Root',
-  fields: () => ({
-    viewer: {
-      type: UserType,
-      resolve: () => EmptyUser
-    }
-  })
+	name: 'Root',
+	fields: () => ({
+		viewer: {
+			type: UserType,
+			resolve: () => EmptyUser
+		}
+	})
 });
 
 export default RootType;
