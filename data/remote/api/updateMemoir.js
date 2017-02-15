bc8f10e05f9b9b8d119bbac7f1ab8d696e406c54
n8e <godmetweenciati@gmail.com>
2017-02-15 19:18:39 +0300

    Changes mongoose findOneAndUpdate to promise resolved using exec

--- data/remote/api/updateMemoir.js
+++ data/remote/api/updateMemoir.js
@@ -1,29 +1,20 @@
 import Memoir from '../models/memoirs';
 
 export function updateMemoir(args) {
-	console.log('args', args);
-	return args;
-  // TODO: Include the req.decoded.id as part of the memoir object "ownerId"
-	// let updatedMemoir = new Memoir({
-	// 	title: args.title,
-	// 	content: args.content,
-	// });
-  //
-  // let promise = Memoir.find({"_id": args.memoirId}).exec();
-  //
-  // promise
-  //   .then(response => {
-  //     if(!response) {
-  //       throw { 'status': 404, 'message': 'Memoir not Found!' };
-  //     }
-  //
-  //     let p = Memoir.findOneAndUpdate({"_id": args.memoirId}, updatedMemoir, updatedMemoir).exec();
-  //
-  //     p.then(resp => {
-  //       console.log('ULTIMATE resp', resp);
-  //       return { memoir: resp };
-  //     })
-  //     .catch(err => new Error(err));
-  //   })
-  //   .catch(err => new Error(err));
+	let promise = Memoir.find({'_id': args.memoirId}).exec();
+
+	return promise.then(response => {
+		if (!response) {
+			throw {'status': 404, 'message': 'Memoir not Found!'};
+		}
+
+		let objectId = response[0]._id;
+
+		return Memoir.findOneAndUpdate({'_id': objectId}, {
+			title: args.title,
+			content: args.content
+		}, {
+			new: true
+		}).exec();
+	}).catch(err => new Error(err));
 }
